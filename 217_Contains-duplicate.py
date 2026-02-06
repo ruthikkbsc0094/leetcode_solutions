fab7b044419aa1e39803d3e78b6f1596e0e87b76
RUTHICK.K <ruthikkbsc24@rvu.edu.in>
2026-02-06 09:31:58 +0530

    Solved problem using bruteforce
    Commented out brute force implementation of containsDuplicate.

---
+++ 217_Contains-duplicate.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        
+        return False
+
+
+# ## using BruteForce
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         seen = set()
+        
+#         for num in nums:
+#             if num in seen:
+#                 return True
+#             seen.add(num)
+        
+#         return False
